9cc1680479a2d1e8efc04220b0be63dad241d468
Dywinar <144463363+Dywinar@users.noreply.github.com>
2023-12-26 15:21:04 +0300

    Update project_1.py

--- project_1.py
+++ project_1.py
@@ -1,10 +1,10 @@
 import pandas as pd
 data = pd.read_csv('/content/Housing.csv')
-data[data['price'] == data['price'].min()]['bedrooms'].min()
+print(data[data['price'] == data['price'].min()]['bedrooms'].min())
 #Вопрос 1: 2
-len(data[data['bedrooms'] <= data['bathrooms']])
+print(len(data[data['bedrooms'] <= data['bathrooms']]))
 #Вопрос 2: 15
-data[data['guestroom'] == 'yes']['price'].min()
+print(data[data['guestroom'] == 'yes']['price'].min())
 #Вопрос 3: 2450000
 data_for_cena = data[((data['price'] >= 500000) | (data['price'] <= 200000))]
 data_with_airconditioning = data_for_cena[data_for_cena['airconditioning']=='yes']
